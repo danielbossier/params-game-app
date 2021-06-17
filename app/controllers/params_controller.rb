826d6f531550610fa478bc8e867c9b4bda90271e
danielbossier <dbossier21@gmail.com>
2021-06-17 10:57:37 -0700

    added response for names with a

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,2 +1,10 @@
 class ParamsController < ApplicationController
+  def name
+    input_value = params["name"]
+    if input_value[0].upcase == "A"
+      render plain: "Hey, #{input_value} your name starts with the first letter of the alphabet."
+    else
+      render json: { name: "Your name is #{input_value}" }
+    end
+  end
 end

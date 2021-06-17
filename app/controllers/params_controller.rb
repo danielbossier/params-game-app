2eaff5a6d7ceebcd5fb9f809552c61a2f46113c3
danielbossier <dbossier21@gmail.com>
2021-06-17 11:16:23 -0700

    added number guessing game

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -7,4 +7,17 @@ class ParamsController < ApplicationController
       render json: { name: "Your name is #{input_value}" }
     end
   end
+
+  def number_game
+    answer = 12
+    input_value = params["number"].to_i
+    if input_value > 12
+      output_message = "That's too high!"
+    elsif input_value < 12
+      output_message = "That's too low!"
+    else
+      output_message = "12 is correct!"
+    end
+    render json: { message: output_message }
+  end
 end
